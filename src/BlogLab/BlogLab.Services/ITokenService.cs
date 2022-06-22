074ba453e05cb11291d82af3d54a762667f77d89
rajbanshirahul <rahul.rajbanshi17@gmail.com>
2022-06-22 21:55:07 +0545

    Add: Token service for Jwt auth

---
+++ src/BlogLab/BlogLab.Services/ITokenService.cs
@@ -0,0 +1,14 @@
+﻿using BlogLab.Models.Account;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogLab.Services
+{
+    public interface ITokenService
+    {
+        public string CreateJwtToken(ApplicationUserIdentity user);
+    }
+}
---
+++ src/BlogLab/BlogLab.Services/TokenService.cs
@@ -0,0 +1,45 @@
+﻿using BlogLab.Models.Account;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace BlogLab.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly IConfiguration _config;
+
+        public TokenService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public string CreateJwtToken(ApplicationUserIdentity user)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var issuer = _config["Jwt:Issuer"];
+
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.ApplicationUserId.ToString()),
+                new Claim(JwtRegisteredClaimNames.UniqueName, user.Username)
+            };
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                issuer,
+                issuer,
+                claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: creds
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
