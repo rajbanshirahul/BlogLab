262739faa4c053f16b14de373d5edd72ba221fdc
rajbanshirahul <rahul.rajbanshi17@gmail.com>
2023-03-10 20:30:18 +0545

    Add: services

--- src/BlogLab/BlogLab-UI/src/app/app.module.ts
+++ src/BlogLab/BlogLab-UI/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -44,9 +45,10 @@ import { RegisterComponent } from './components/register/register.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
