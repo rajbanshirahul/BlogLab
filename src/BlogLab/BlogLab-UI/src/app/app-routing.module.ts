2003b960e9b69e0b693f47b64231b3900b8cade9
rajbanshirahul <rahul.rajbanshi17@gmail.com>
2023-03-11 16:29:19 +0545

    Add: auth guards and routing

--- src/BlogLab/BlogLab-UI/src/app/app-routing.module.ts
+++ src/BlogLab/BlogLab-UI/src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogEditComponent } from './components/blog/blog-edit/blog-edit.component';
+import { BlogComponent } from './components/blog/blog/blog.component';
+import { BlogsComponent } from './components/blog/blogs/blogs.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { PhotoAlbumComponent } from './components/photo-album/photo-album.component';
+import { AuthGuard } from './guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'blogs', component: BlogsComponent },
+  { path: 'blogs/:id', component: BlogComponent },
+  { path: 'photo-album', component: PhotoAlbumComponent, canActivate: [AuthGuard] },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'dashboard/:id', component: BlogEditComponent, canActivate: [AuthGuard] },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/BlogLab/BlogLab-UI/src/app/guards/auth.guard.ts
+++ src/BlogLab/BlogLab-UI/src/app/guards/auth.guard.ts
@@ -1,15 +1,30 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { AccountService } from '../services/account.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+
+  constructor(
+    private accountService: AccountService,
+    private router: Router
+  ) { }
+
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot): boolean {
+
+    const currentUser = this.accountService.currentUserValue;
+    const isLoggedIn = currentUser && currentUser.token;
+
+    if (isLoggedIn) {
+      return true;
+    }
+
+    this.router.navigate(['/']);
+    return false;
   }
-  
+
 }
