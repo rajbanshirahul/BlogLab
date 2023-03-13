68191eeffb23feaa943df3a2517b0846f0f5d292
rajbanshirahul <rahul.rajbanshi17@gmail.com>
2023-03-13 21:45:53 +0545

    Fix: syntax and typos

--- src/BlogLab/BlogLab-UI/src/app/interceptors/error.interceptor.ts
+++ src/BlogLab/BlogLab-UI/src/app/interceptors/error.interceptor.ts
@@ -8,12 +8,14 @@ import {
 import { catchError, Observable, throwError } from 'rxjs';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../services/account.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
   constructor(
     private toastr: ToastrService,
+    private router: Router,
     private accountService: AccountService) { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
@@ -53,7 +55,7 @@ export class ErrorInterceptor implements HttpInterceptor {
 
       this.toastr.error(errorMessage, error.statusText);
       console.log(error.error);
-    } else if (!!error?.error.errors.Content && (typeof error.error.errors.Content) === 'object') {
+    } else if (!!error?.error?.errors?.Content && (typeof error.error.errors.Content) === 'object') {
       let errorObject = error.error.errors.Content;
       let errorMessage = '';
 
@@ -65,7 +67,7 @@ export class ErrorInterceptor implements HttpInterceptor {
       this.toastr.error(errorMessage, error.statusCode);
       console.log(error.error);
     } else if (!!error.error) {
-      let errorMessage = ((typeof error.error) == 'string')
+      let errorMessage = ((typeof error.error) === 'string')
         ? error.error
         : 'There was a validation error.';
 
@@ -79,7 +81,7 @@ export class ErrorInterceptor implements HttpInterceptor {
     let errorMessage = 'Please login to your account.';
     this.accountService.logout();
     this.toastr.error(errorMessage, error.statusText);
-    // route to login page
+    this.router.navigate(['/login']);
   }
 
   handle500Error(error: any) {
